12330821e1f1333013e898a12c0e8f003fbbb7db
tsukasa kuribayashi <tsukasa.kuribayashi@techacademy.jp>
2021-01-06 03:13:25 +0000

    groups_controller_spec

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,22 +1,36 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe GroupsController, type: :controller do
-#   before do
-#     @user = FactoryBot.create(:user)
-#     @another_user = FactoryBot.create(:another_user)
-#   end
-  
-#   describe "#index" do
-#     # 正常なレスポンスか？
-#     it "responds successfully" do
-#       get :index
-#       expect(response).to be_success
-#     end
-#     # 200レスポンスが返ってきているか？
-#     it "returns a 200 response" do
-#       get :index
-#       expect(response).to have_http_status "200"
-#     end
-#   end
+RSpec.describe GroupsController, type: :controller do
+    
+    before do
+        @user = FactoryBot.create(:user)
+        session[:user_id] = @user.id
+        @group = FactoryBot.create(:group)
+    end
+    
+    describe "#index" do
+        # 正常なレスポンスか？
+        it "responds successfully" do
+          get :index
+          expect(response).to be_success
+        end
+        # 200レスポンスが返ってきているか？
+        it "returns a 200 response" do
+          get :index
+          expect(response).to have_http_status "200"
+        end
+    end
 
-# end
+    describe "#show" do
+      # 正常なレスポンスか？
+      it "responds successfully" do
+        get :show, params: {id: @group.id}
+        expect(response).to be_success
+      end
+      # 200レスポンスが返ってきているか？
+      it "returns a 200 response" do
+        get :show, params: {id: @group.id}
+        expect(response).to have_http_status "200"
+      end
+    end
+end
