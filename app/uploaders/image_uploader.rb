67e87c4cbdd81f8de1a84471ac79a384d12440b5
tsukasa kuribayashi <tsukasa.kuribayashi@techacademy.jp>
2021-01-30 09:04:21 +0000

    remove 'storage :file' from image_uploader.rb because the phrases are written twice in the file

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,10 +3,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   if Rails.env.production?
