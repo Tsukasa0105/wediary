b888ea8639127529ed3b436b2ceae3e98f4b155c
tsukasa kuribayashi <tsukasa.kuribayashi@techacademy.jp>
2021-02-16 03:57:27 +0000

    modify validation of memo.title from 'minimum' to 'maximum'. '

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -5,7 +5,7 @@ class Memo < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   
-  validates :title, presence: true, length: { minimum: 10 }
+  validates :title, presence: true, length: { maximum: 10 }
   
   def favorite_by(user)
       favorites.find_by(user_id: user.id)
